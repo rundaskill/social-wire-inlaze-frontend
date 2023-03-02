df9121efabc972f3ad664ee8517286bb49bd95f7
Rundaskill <danielxi@hotmail.es>
2023-03-02 15:51:27 -0500

    regresar

--- src/app/layout/full/appmain/appmain.component.ts
+++ src/app/layout/full/appmain/appmain.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./appmain.component.scss']
 })
 export class AppmainComponent implements OnInit {
-  @Output() activateView:EventEmitter<"seeAll">=new EventEmitter<"seeAll">()
+ 
 
   constructor() { }
 
